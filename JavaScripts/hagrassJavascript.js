3f870b698c57565c7ff6f04bf03b4773abeea1f2
HAGRASS2001 <83500189+HAGRASS2001@users.noreply.github.com>
2022-03-30 20:31:41 +0200

    order history sort and search + some design done

--- JavaScripts/hagrassJavascript.js
+++ JavaScripts/hagrassJavascript.js
@@ -46,3 +46,22 @@ function sortTable() {
     }
   }
 }
+
+function searchInTable() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("orderHistory");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}

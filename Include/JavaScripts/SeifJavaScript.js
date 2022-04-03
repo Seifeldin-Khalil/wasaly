a9ccf785fab402ce54db70d004e30ca4e12d3434
Seifeldin-Khalil <96579102+Seifeldin-Khalil@users.noreply.github.com>
2022-04-03 21:51:45 +0200

    fadel js w sowar

--- Include/JavaScripts/SeifJavaScript.js
+++ Include/JavaScripts/SeifJavaScript.js
@@ -1,3 +1,19 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
 
@@ -13,4 +29,7 @@ function cartClick(){
 let button =this;
 button.classList.add('clicked');
 }
+
+
+
 });

04a7fe1c6cae91e7e8b6cded44d05f13aa9c1898
Seifeldin-Khalil <96579102+Seifeldin-Khalil@users.noreply.github.com>
2022-04-05 15:55:37 +0200

    ya rab

--- Include/JavaScripts/SeifJavaScript.js
+++ Include/JavaScripts/SeifJavaScript.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function(event) {
 
 
@@ -21,9 +20,61 @@ function yesnoCheck() {
         document.getElementById('ifYes').style.visibility = 'visible';
     }
     else document.getElementById('ifYes').style.visibility = 'hidden';
-
 }
 
 function paymentAlert() {
   alert("Payment Successful");
+}
+
+function checkoutValidation() {
+    var x = document.forms["Checkout"]["yes"];
+     if (x.checked) {
+     var x = document.forms["Checkout"]["City"].value;
+  if (x == "") {
+    alert("City must be filled out");
+    return false;
+  }
+    var x = document.forms["Checkout"]["Address"].value;
+  if (x == "") {
+    alert("Address must be filled out");
+    return false;
+  }
+      var x = document.forms["Checkout"]["postal"].value;
+  if (x == "") {
+    alert("Post/zip code must be filled out");
+    return false;
+  }
+       var x = document.forms["Checkout"]["cName"].value;
+  if (x == "") {
+    alert("Card Name Holder must be filled out");
+    return false;
+  }
+         var x = document.forms["Checkout"]["cvv"].value;
+  if (x == "") {
+    alert("cvv must be filled out");
+    return false;
+  }
+        var x = document.forms["Checkout"]["Expiry"].value;
+  if (x == "") {
+    alert("Expiry Date must be filled out");
+    return false;
+  }
+}
+    else{
+        var x = document.forms["Checkout"]["City"].value;
+  if (x == "") {
+    alert("City must be filled out");
+    return false;
+  }
+    var x = document.forms["Checkout"]["Address"].value;
+  if (x == "") {
+    alert("Address must be filled out");
+    return false;
+  }
+      var x = document.forms["Checkout"]["postal"].value;
+  if (x == "") {
+    alert("Post/zip code must be filled out");
+    return false;
+  }
+    }
 }
